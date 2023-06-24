a87d0ecff48eaa9499eb7ff22b247bc4401114a5
Noman <noman@noman.land>
2023-06-24 13:59:59 -0400

    Add react app

---
+++ src/transcript-app/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App(): JSX.Element {
+  return (
+    <>
+      <h1>transcript.fish</h1>
+      <img src="https://upload.wikimedia.org/wikipedia/en/e/e1/No_Such_Thing_As_A_Fish_logo.jpg" />
+    </>
+  );
+}
+
+export default App;
