2f780d2b390a925f719cc6a7d463072b340097ba
Noman <noman@noman.land>
2023-06-24 19:55:56 -0400

    Include image assets in build

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  assetsInclude: ["/images"],
   plugins: [react(), tsconfigPaths()],
 });
