f13c6d6295401a18994506c05aca5af5e8e25bba
Noman <noman@noman.land>
2023-06-24 19:58:28 -0400

    Add some boilerplate

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,20 @@
-export const App = () => (
-  <>
-    <h1>transcript.fish</h1>
-    <img src="https://upload.wikimedia.org/wikipedia/en/e/e1/No_Such_Thing_As_A_Fish_logo.jpg" />
-  </>
-);
+import { useEffect, useState } from "react";
+
+export const App = () => {
+  const [episodes] = useState([]);
+  useEffect(() => {
+    fetch("").then().catch();
+  }, []);
+
+  return (
+    <>
+      <h1>transcript.fish</h1>
+      <img src="/images/logo.jpg" />
+      <ul>
+        {episodes.map((episode) => (
+          <li>{episode}</li>
+        ))}
+      </ul>
+    </>
+  );
+};
