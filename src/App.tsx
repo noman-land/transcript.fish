fc3321ffc616126ccc17b6521debaf56042ff2df
Noman <noman@noman.land>
2023-06-24 23:59:42 -0400

    Add Github link

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,45 @@
 import { Episodes } from "./Episodes";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  header {
+    height: 32px;
+    display: flex;
+    justify-content: end;
+  }
+
+  .content {
+    flex-grow: 1;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+`;
 
 export const App = () => {
   return (
-    <>
-      <h1>transcript.fish</h1>
-      <h2>coming soon</h2>
-      <img src="https://upload.wikimedia.org/wikipedia/en/e/e1/No_Such_Thing_As_A_Fish_logo.jpg" />
-      {/* <img src="/images/logo.jpg" /> */}
-      <Episodes />
-    </>
+    <Wrapper>
+      <header>
+        <a href="https://github.com/noman-land/transcript.fish">
+          <img
+            width={32}
+            src="https://logos-download.com/wp-content/uploads/2016/09/GitHub_logo.png"
+          />
+        </a>
+      </header>
+      <div className="content">
+        <h1>transcript.fish</h1>
+        <h2>coming soon</h2>
+        <img
+          width={316}
+          src="https://upload.wikimedia.org/wikipedia/en/e/e1/No_Such_Thing_As_A_Fish_logo.jpg"
+        />
+        <Episodes />
+      </div>
+    </Wrapper>
   );
 };
