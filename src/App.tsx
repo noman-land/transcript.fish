e08ce3badee2ab8ff45f806de79e47c104d267ee
Noman <noman@noman.land>
2023-06-24 20:03:35 -0400

    Fix some things

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,14 @@ import { useEffect, useState } from "react";
 export const App = () => {
   const [episodes] = useState([]);
   useEffect(() => {
-    fetch("").then().catch();
+    // fetch('').then().catch();
   }, []);
 
   return (
     <>
       <h1>transcript.fish</h1>
-      <img src="/images/logo.jpg" />
+      <img src="https://upload.wikimedia.org/wikipedia/en/e/e1/No_Such_Thing_As_A_Fish_logo.jpg" />
+      {/* <img src="/images/logo.jpg" /> */}
       <ul>
         {episodes.map((episode) => (
           <li>{episode}</li>
