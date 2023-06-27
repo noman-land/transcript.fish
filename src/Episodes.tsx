2aee9bab7e5dc9ed0ad83846e6900fe1349e944e
Noman <noman@noman.land>
2023-06-27 11:53:41 -0400

    Fix type

--- src/Episodes.tsx
+++ src/Episodes.tsx
@@ -3,28 +3,24 @@ import { useDb } from "./dbHooks";
 const formatDate = (date: string) =>
   date ? new Intl.DateTimeFormat().format(new Date(date)) : "";
 
+interface Episode {
+  episode: number;
+  title: string;
+  pubDate: string;
+}
+
 export const Episodes = () => {
   const episodes = useDb();
   return (
     <table>
       <tbody>
-        {episodes.map(
-          ({
-            episode,
-            title,
-            pubDate,
-          }: {
-            episode: number;
-            title: string;
-            pubDate: string;
-          }) => (
-            <tr key={episode}>
-              <td style={{ textAlign: "right" }}>{episode}:</td>
-              <td style={{ textAlign: "left" }}>{title}</td>
-              <td style={{ textAlign: "right" }}>{formatDate(pubDate)}</td>
-            </tr>
-          )
-        )}
+        {episodes.map(({ episode, title, pubDate }: Episode) => (
+          <tr key={episode}>
+            <td style={{ textAlign: "right" }}>{episode}:</td>
+            <td style={{ textAlign: "left" }}>{title}</td>
+            <td style={{ textAlign: "right" }}>{formatDate(pubDate)}</td>
+          </tr>
+        ))}
       </tbody>
     </table>
   );
