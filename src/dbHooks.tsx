b8ef0e824a01ae2a1df82b1513b1e8d93cd3aa8a
Noman <noman@noman.land>
2023-06-27 21:40:21 -0400

    Fix things

--- src/dbHooks.tsx
+++ src/dbHooks.tsx
@@ -1,5 +1,5 @@
-import { useCallback, useEffect, useState } from "react";
-import { createDbWorker } from "sql.js-httpvfs";
+import { useCallback, useEffect, useRef, useState } from "react";
+import { WorkerHttpvfs, createDbWorker } from "sql.js-httpvfs";
 
 const workerUrl = new URL(
   "sql.js-httpvfs/dist/sqlite.worker.js",
@@ -9,31 +9,50 @@ const wasmUrl = new URL("sql.js-httpvfs/dist/sql-wasm.wasm", import.meta.url);
 
 const maxBytesToRead = 10 * 1024 * 1024;
 
-const worker = await createDbWorker(
-  [
-    {
-      from: "inline",
-      config: {
-        requestChunkSize: 4 * 1024,
-        serverMode: "full",
-        url: "https://media.transcript.fish/transcript.db",
-      },
-    },
-  ],
-  workerUrl.toString(),
-  wasmUrl.toString(),
-  maxBytesToRead // optional, defaults to Infinity
-);
-
 export const useDb = () => {
+  const workerRef = useRef<WorkerHttpvfs>();
   const [episodes, setEpisodes] = useState([]);
   const [episodeWords, setEpisodeWords] = useState([]);
 
+  useEffect(() => {
+    createDbWorker(
+      [
+        {
+          from: "inline",
+          config: {
+            requestChunkSize: 4 * 1024,
+            serverMode: "full",
+            url: "https://media.transcript.fish/transcript.db",
+          },
+        },
+      ],
+      workerUrl.toString(),
+      wasmUrl.toString(),
+      maxBytesToRead // optional, defaults to Infinity
+    ).then((worker) => {
+      workerRef.current = worker;
+      workerRef.current.db
+        .query(
+          `select episode, title, pubDate, image, description, duration from episodes`
+        )
+        .then(
+          (value) => setEpisodes(value as []),
+          (err) => console.error("Error selecting from db:", err)
+        )
+        .catch((err) =>
+          console.error(
+            "Something absolutely terrible and unexpected happened getting episodes from database.",
+            err
+          )
+        );
+    });
+  }, []);
+
   const getEpisode = useCallback((episode: number) => {
-    worker.db
+    workerRef.current?.db
       .query("select count(*) from words where episode == ?", [episode])
       .then((value) => setEpisodeWords(value as []))
-      .catch((err) =>
+      .catch((err: Error) =>
         console.error(
           "Something absolutely terrible and unexpected happened getting episode words from database.",
           err
@@ -41,21 +60,5 @@ export const useDb = () => {
       );
   }, []);
 
-  useEffect(() => {
-    worker.db
-      .query(
-        `select episode, title, pubDate, image, description, duration from episodes`
-      )
-      .then(
-        (value) => setEpisodes(value as []),
-        (err) => console.error("Error selecting from db:", err)
-      )
-      .catch((err) =>
-        console.error(
-          "Something absolutely terrible and unexpected happened getting episodes from database.",
-          err
-        )
-      );
-  }, []);
   return { episodes, episodeWords, getEpisode };
 };
