c70abde63c89f16b79716379f576c05b9d7e5535
noman <27938023+noman-land@users.noreply.github.com>
2023-07-22 01:37:58 -0400

    feat: Make more mobile friendly (#74)
    * Break links in descriptions so they don't overflow
    
    * Align text
    
    * Make more mobile friendly

---
+++ src/UnderConstructionBanner.tsx
@@ -0,0 +1,41 @@
+import { styled } from 'styled-components';
+
+const BannerWrapper = styled.div`
+  font-size: 0.8em;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-bottom: 1.5em;
+  text-align: center;
+
+  h2 {
+    margin-left: 1vw;
+    margin-right: 1vw;
+  }
+
+  .construction-sign {
+    width: 50px;
+
+    @media (max-width: 400px) {
+      width: 40px;
+    }
+    @media (max-width: 360px) {
+      width: 32px;
+    }
+    @media (max-width: 320px) {
+      width: 24px;
+    }
+  }
+`;
+
+const ConstructionSign = () => (
+  <img className="construction-sign" src="/images/construction-sign.gif" />
+);
+
+export const UnderConstructionBanner = () => (
+  <BannerWrapper>
+    <ConstructionSign />
+    <h2>under construction</h2>
+    <ConstructionSign />
+  </BannerWrapper>
+);
