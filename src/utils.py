70f4049f934dbfe1457cef1fb29c6e33c7787e6f
Noman <noman@noman.land>
2023-07-25 05:16:39 -0400

    Move around files

--- src/utils.py
+++ src/utils.py
@@ -8,12 +8,12 @@ def now():
     return datetime.now().strftime('%H:%M:%S')
 
 def make_audio_file_path(episode_num):
-    return f'public/audio/{episode_num}.mp3'
+    return f'audio/{episode_num}.mp3'
 
 def make_image_file_path(episode_num, image_url):
     file_extension = os.path.splitext(image_url)[1]
     # file extension includes dot at the beginning
-    return f'public/images/episodes/{episode_num}{file_extension}'
+    return f'images/episodes/{episode_num}{file_extension}'
 
 def get_episode_num(episode):
     return int(episode['itunes_episode'])
