8c38a5f26a5b86ad729f2114536e13c70f35b92e
Noman <noman@noman.land>
2023-07-25 05:05:00 -0400

    Undo adding files

---
+++ src/main.py
@@ -0,0 +1,14 @@
+import database
+import sys
+from convert import convert
+
+# All of this is idempotent
+# Run it as many times as you you like
+# It will fill in any gap
+if __name__ == '__main__':
+    try:
+        convert()
+    except KeyboardInterrupt:
+        print('\n[CTRL + C DETECTED] Script interrupted. Cleaning up.')
+        database.close()
+        sys.exit(0)
