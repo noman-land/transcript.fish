fed82df116e857e1ac9c7f5d57b5756156b0bccc
Noman <noman@noman.land>
2023-08-07 03:10:03 -0400

    Fix

--- scripts/create_db.sh
+++ scripts/create_db.sh
@@ -1,30 +1,32 @@
 set -eu
 
 db_path="./db/transcript.db"
+# last modified date of database
 latest=$(date -r "$db_path" "+%s")
-echo '{ "latest": '$latest' }' > ./db/latest.json
 output_dir="./db/$latest"
 
 if ! test -d "$output_dir"; then
-  mkdir "$output_dir";
-  # for chunked mode, we need to know the database size in bytes beforehand
+  # database size in bytes for chunked mode
   bytes="$(stat -f "%z" "$db_path")"
-  # set chunk size to 4MiB (needs to be a multiple of the `pragma page_size`!)
+  # size of database chunk files. needs to be a multiple of `pragma page_size`
   server_chunk_size=$((4 * 1024 * 1024))
-  suffix_length=3
-  rm -f "$output_dir/db.sqlite3"*
-  split -d -b $server_chunk_size -a $suffix_length "$db_path" "$output_dir/db.sqlite3."
-
   # set request chunk size to match page size
   request_chunk_size="$(sqlite3 "$db_path" 'pragma page_size')"
+  url_prefix="db.sqlite3."
+  suffix_length=3
 
-  # write a json config
+  mkdir "$output_dir";
+  split -d -b $server_chunk_size -a $suffix_length "$db_path" "$output_dir/$url_prefix"
+
+  # write timestamp of database modified date for FE cache busting
+  echo '{ "latest": '$latest' }' > ./db/latest.json
+  # write json config for database
   echo '{
     "serverMode": "chunked",
     "requestChunkSize": '$request_chunk_size',
     "databaseLengthBytes": '$bytes',
     "serverChunkSize": '$server_chunk_size',
-    "urlPrefix": "db.sqlite3.",
+    "urlPrefix": '$url_prefix',
     "suffixLength": '$suffix_length'
   }' > "$output_dir/config.json"
 else
