08be194e07416c8ee3f59858acf4d963c4eed441
Noman <noman@noman.land>
2023-09-08 03:28:24 -0400

    Smoothen logs

--- src/python/whisper.py
+++ src/python/whisper.py
@@ -27,10 +27,10 @@ def transcribe(episode):
     episode_num = utils.get_episode_num(episode)
     saved_word_count = database.select_word_count(episode_num)
     if saved_word_count > 0:
-        utils.log(episode_num, f'Already transcribed with {saved_word_count} words. Skipping.')
+        utils.log(episode_num, f'Already complete: transcription: {saved_word_count} words')
         return 0
 
-    utils.log(episode_num, 'Starting transcription.')
+    utils.log(episode_num, 'Starting: transcription')
     word_count = 0
     for segment in get_transcription_segments(episode):
         words = getattr(segment, 'words', [])
@@ -38,5 +38,5 @@ def transcribe(episode):
         database.insert_words(episode_num, words)
     database.upsert_episode(episode, word_count)
     database.commit()
-    utils.log(episode_num, f'Transcription complete with {word_count} words.')
+    utils.log(episode_num, f'Completed: transcription: {word_count} words.')
     return 1
