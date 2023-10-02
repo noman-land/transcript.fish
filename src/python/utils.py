0893e62b7cef519d003000f1a2cd473fff32ec4b
lilyMrt <lily.pinson@gmail.com>
2023-10-02 13:08:24 +1100

    Move trailing slash to path building string

--- src/python/utils.py
+++ src/python/utils.py
@@ -2,8 +2,8 @@ from datetime import datetime
 import os
 from bs4 import BeautifulSoup
 
-AUDIO_PATH = "audio/"
-IMAGE_PATH = "images/episodes/"
+AUDIO_PATH = "audio"
+IMAGE_PATH = "images/episodes"
 
 def log(episode_num, *msg):
     print(f'-- {now()} [ Episode {episode_num} ]', *msg)
@@ -18,12 +18,12 @@ def create_file_path(folder_path):
         print(f'-- {now()} Created folder {folder_path}')
 
 def make_audio_file_path(episode_num):
-    return f'{AUDIO_PATH}{episode_num}.mp3'
+    return f'{AUDIO_PATH}/{episode_num}.mp3'
 
 def make_image_file_path(episode_num, image_url):
     file_extension = os.path.splitext(image_url)[1]
     # file extension includes dot at the beginning
-    return f'{IMAGE_PATH}{episode_num}{file_extension}'
+    return f'{IMAGE_PATH}/{episode_num}{file_extension}'
 
 def get_episode_num(episode):
     return int(episode['itunes_episode'])
