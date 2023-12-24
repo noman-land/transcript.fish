b191ad6b4c245a06426a92b879bcc9d6785bcf49
noman <27938023+noman-land@users.noreply.github.com>
2023-12-24 17:43:42 -0500

    chore: Clean up total section (#249)

--- src/js/filters/FilterBar.tsx
+++ src/js/filters/FilterBar.tsx
@@ -1,9 +1,9 @@
 import styled from 'styled-components';
+import { useState } from 'react';
 import { SearchFilters } from './SearchFilters';
 import { EpisodeTypeFilters } from './EpisodeTypeFilters';
 import { PresenterFilters } from './PresenterFilters';
 import { FiltersToggle } from './FiltersToggle';
-import { useState } from 'react';
 
 const StyledWrapper = styled.div`
   display: flex;
@@ -12,19 +12,6 @@ const StyledWrapper = styled.div`
   flex-wrap: wrap;
   margin: 0 3vw;
 
-  .filters {
-    margin-left: 2.4rem;
-    margin-bottom: 1rem;
-  }
-
-  .filters > :first-child {
-    margin-top: 1rem;
-  }
-
-  .filters > :not(:first-child) {
-    margin-top: 1.8rem;
-  }
-
   @media (max-width: 900px) {
     margin: 0 4.5vw;
   }
@@ -32,10 +19,18 @@ const StyledWrapper = styled.div`
   @media (max-width: 650px) {
     margin: 0 6vw;
   }
+`;
 
-  @media (max-width: 480px) {
-    flex-direction: column;
-    align-items: stretch;
+const FiltersWrapper = styled.div`
+  margin-left: 2.4rem;
+  margin-bottom: 1rem;
+
+  & > :first-child {
+    margin-top: 1rem;
+  }
+
+  & > :not(:first-child) {
+    margin-top: 1.8rem;
   }
 `;
 
@@ -45,11 +40,11 @@ export const FilterBar = () => {
     <StyledWrapper>
       <FiltersToggle isOpen={isOpen} onToggle={() => setIsOpen(o => !o)} />
       {isOpen && (
-        <div className="filters">
+        <FiltersWrapper>
           <SearchFilters />
           <EpisodeTypeFilters />
           <PresenterFilters />
-        </div>
+        </FiltersWrapper>
       )}
     </StyledWrapper>
   );
