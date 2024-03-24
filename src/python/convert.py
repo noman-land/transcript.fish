1a8e70c3dd876c304afd2a7f58583520ea917a29
Noman <noman@noman.land>
2024-03-24 00:42:08 -0400

    Rename variable

--- src/python/convert.py
+++ src/python/convert.py
@@ -3,12 +3,12 @@ import fetch
 import whisper
 import utils
 
-def convert(episode_num_to_reconvert: int | None):
+def convert(episode_num: int | None):
     transcribed = 0
-    for rss_episode in fetch.get_rss_episodes(episode_num_to_reconvert):
+    for rss_episode in fetch.get_rss_episodes(episode_num):
         db_episode = database.select_episode(rss_episode.episode_num)
         new_episode_is_shorter = rss_episode.duration < db_episode.duration
-        if episode_num_to_reconvert or new_episode_is_shorter:
+        if episode_num or new_episode_is_shorter:
             if new_episode_is_shorter:
                 utils.log(db_episode.episode_num, 'Shorter episode found')
             utils.delete_old_episode(db_episode)
