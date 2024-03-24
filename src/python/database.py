850e10fd0f0157f92cdb68595d886292e965bf00
Noman <noman@noman.land>
2024-03-24 17:24:07 -0400

    Tweaks

--- src/python/database.py
+++ src/python/database.py
@@ -70,10 +70,11 @@ def select_episode(episode_num: int):
         FROM
             episodes
         WHERE
-            episode = ?
+            episode = ?;
     '''
     cur = con.cursor()
-    return DbEpisode(cur.execute(select_word_count_sql, [episode_num]).fetchone())
+    result = cur.execute(select_word_count_sql, [episode_num]).fetchone()
+    return DbEpisode(result)
 
 def select_word_count(episode_num: int) -> int:
     select_word_count_sql = '''
@@ -84,7 +85,7 @@ def select_word_count(episode_num: int) -> int:
         WHERE
             episode = ?
         GROUP BY
-            episode
+            episode;
     '''
     cur = con.cursor()
     result = cur.execute(select_word_count_sql, [episode_num]).fetchone()
@@ -96,7 +97,7 @@ def delete_transcription(episode_num: int):
         DELETE FROM
             words
         WHERE
-            episode = ?
+            episode = ?;
     '''
     reset_word_count_sql = '''
         UPDATE
@@ -104,7 +105,7 @@ def delete_transcription(episode_num: int):
         SET
             wordCount = 0
         WHERE
-            episode = ?
+            episode = ?;
     '''
     cur = con.cursor()
     cur.execute(delete_words_sql, [episode_num])
@@ -125,7 +126,7 @@ def upsert_episode(episode: RssEpisode, word_count: int):
         DO UPDATE SET
             audio = excluded.audio,
             duration = excluded.duration,
-            wordCount = excluded.wordCount
+            wordCount = excluded.wordCount;
     '''
     episode_row = make_episode_row(episode, word_count)
     cur = con.cursor()
