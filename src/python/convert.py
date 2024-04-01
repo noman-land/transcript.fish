51d080df661be9809053d262757b12faa7c0e6a6
noman <27938023+noman-land@users.noreply.github.com>
2024-03-31 21:08:06 -0400

    fix: Handle new episodes (#286)

--- src/python/convert.py
+++ src/python/convert.py
@@ -2,22 +2,27 @@ import database
 import fetch
 import utils
 import whisper
+from classes import RssEpisode
 from datetime import datetime
 from typing import Optional
+
+def maybe_cleanup(rss_episode: RssEpisode, episode_num: Optional[int]):
+    existing_episode = database.select_episode(rss_episode.episode_num)
+    if existing_episode:
+        new_episode_is_shorter = rss_episode.duration < existing_episode.duration
+        if episode_num or new_episode_is_shorter:
+            if new_episode_is_shorter:
+                utils.log(existing_episode.episode_num, 'Shorter episode found')
+            database.delete_transcription(existing_episode.episode_num)
+            utils.delete_audio(existing_episode.episode_num)
+            utils.log(existing_episode.episode_num, 'Redownloading and retranscribing')
 
 def convert(episode_num: Optional[int]):
     transcribed = 0
     for rss_episode in fetch.get_rss_episodes(episode_num):
         start_time = datetime.now()
-        db_episode = database.select_episode(rss_episode.episode_num)
-        new_episode_is_shorter = rss_episode.duration < db_episode.duration
-        if episode_num or new_episode_is_shorter:
-            if new_episode_is_shorter:
-                utils.log(db_episode.episode_num, 'Shorter episode found')
-            utils.delete_audio(db_episode.episode_num)
-            database.delete_transcription(db_episode.episode_num)
-            utils.log(db_episode.episode_num, 'Redownloading and retranscribing')
+        maybe_cleanup(rss_episode, episode_num)
         fetch.download_audio(rss_episode)
         fetch.download_image(rss_episode)
         transcribed += whisper.transcribe(rss_episode, start_time)
-    database.close(transcribed > 0)
+    database.close(rebuild_fts=transcribed > 0)
