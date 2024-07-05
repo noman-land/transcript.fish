92edb0ce142babd9e6a0b06ad822b83d2825cc97
noman <27938023+noman-land@users.noreply.github.com>
2024-07-05 01:43:34 -0400

    fix: Each row should fetch its own transcript (#295)

--- src/js/EpisodeRow.tsx
+++ src/js/EpisodeRow.tsx
@@ -1,11 +1,11 @@
 import styled from 'styled-components';
-import { useCallback, useContext, useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { EpisodeRowProps } from './types';
 import { EpisodeSummaryCell } from './EpisodeSummaryCell';
 import { EpisodeTranscriptCell } from './EpisodeTranscriptCell';
 import { makeEpisodeCoverUrl } from './utils';
 import { Colors } from './constants';
-import { DatabaseContext } from './database/DatabaseProvider';
+import { useTranscript } from './database/dbHooks';
 
 const StyledTr = styled.tr<{ $isOpen: boolean }>`
   background-color: ${({ $isOpen }) => $isOpen && Colors.cirtineWhite};
@@ -45,10 +45,7 @@ const TrWithBackground = styled(StyledTr)<{ $image: string }>`
 
 export const EpisodeRow = ({ episode, expanded }: EpisodeRowProps) => {
   const [isOpen, setIsOpen] = useState(false);
-
-  const {
-    transcript: { get: getTranscript, data: transcript },
-  } = useContext(DatabaseContext);
+  const { get: getTranscript, data: transcript } = useTranscript();
 
   const handleClick = useCallback(() => {
     setIsOpen(open => !open);
