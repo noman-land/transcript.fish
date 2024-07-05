6928b06b601fef9263e0cbd15d65dc39f15cd375
Noman <noman@noman.land>
2024-07-05 01:35:10 -0400

    Each row fetches its own transcript

--- src/js/database/DatabaseProvider.tsx
+++ src/js/database/DatabaseProvider.tsx
@@ -2,11 +2,9 @@ import { ReactElement, createContext, useEffect } from 'react';
 import {
   UseEpisodes,
   UsePresenters,
-  UseTranscript,
   UseVenues,
   useEpisodes,
   usePresenters,
-  useTranscript,
   useVenues,
 } from './dbHooks';
 
@@ -14,19 +12,16 @@ export const DatabaseContext = createContext<{
   episodes: Omit<ReturnType<UseEpisodes>, 'get'>;
   presenters: Omit<ReturnType<UsePresenters>, 'get'>;
   venues: Omit<ReturnType<UseVenues>, 'get'>;
-  transcript: ReturnType<UseTranscript>;
 }>({
   episodes: { loading: false, search: () => undefined },
   presenters: { loading: false },
   venues: { loading: false },
-  transcript: { loading: false, get: () => undefined },
 });
 
 export const DatabaseProvider = ({ children }: { children: ReactElement }) => {
   const { get: getEpisodes, ...episodes } = useEpisodes();
   const { get: getPresenters, ...presenters } = usePresenters();
   const { get: getVenues, ...venues } = useVenues();
-  const transcript = useTranscript();
 
   useEffect(() => {
     getPresenters();
@@ -37,7 +32,6 @@ export const DatabaseProvider = ({ children }: { children: ReactElement }) => {
   const value = {
     presenters,
     episodes,
-    transcript,
     venues,
   };
 
